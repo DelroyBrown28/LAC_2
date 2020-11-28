9e33e02223163bd50a968b4b6cf0a03822628fed
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-11-28 22:57:43 +0000

    added base template

--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 
-
 def home(request):
+    if request.user.is_authenticated:
+        username_is = "Delroy Brown"
+        context = {"username_is": username_is}
+    else:
+        context = {"username_is": "Stranger Danger!"}
     template = 'base.html'
-    context = locals()
     return render(request, template, context)
