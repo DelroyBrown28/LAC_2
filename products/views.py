963f9aaffd6abc53bb27ab0c9a453ad81ea64b49
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-11-29 00:02:13 +0000

    added block stuff to templates

--- products/views.py
+++ products/views.py
@@ -7,5 +7,5 @@ def home(request):
         context = {"username_is": username_is}
     else:
         context = {"username_is": "Stranger Danger!"}
-    template = 'base.html'
+    template = 'products/home.html'
     return render(request, template, context)
