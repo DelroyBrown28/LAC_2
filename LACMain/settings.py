85b76a5c62f102317313c2165d1855d11e45155b
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-11-30 17:15:14 +0000

    images now go to static_fille

--- LACMain/settings.py
+++ LACMain/settings.py
@@ -106,5 +106,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join((BASE_DIR), 'static', 'media')
+# MEDIA_ROOT = 'static/media'
 
+STATIC_ROOT = os.path.join((BASE_DIR), 'static', 'static_root')
 
+STATICFILES_DIRS = (
+    os.path.join((BASE_DIR), 'static', 'static_files'),
+)
--- LACMain/urls.py
+++ LACMain/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from products import views
 from django.urls import path
-
 
 
 urlpatterns = [
@@ -10,4 +11,8 @@ urlpatterns = [
     path('products/', views.all, name='products'),
 
 
-]
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
