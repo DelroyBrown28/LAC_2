39f7d0b5d6c1fc9f24709bb3c390ba9f6f0c2ffb
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-12-02 14:30:10 +0000

    added search functionality

--- LACMain/urls.py
+++ LACMain/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('products/', views.all, name='products'),
+    path('s/', views.search, name='search'),
     path('products/<slug:slug>', views.single, name='single_product'),
 
 
